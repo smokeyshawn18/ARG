dbe0c9e81881938d9eda7fe23fb55c130b1f1da9
Shudar_shawn <shudarsanpoudel25@gmail.com>
2024-09-15 09:35:56 +0545

    Messi

--- src/components/Fixture.jsx
+++ src/components/Fixture.jsx
@@ -2,6 +2,7 @@ import Schedule from "./Schedule";
 import Argentina from "../assets/arg.png";
 import Vene from "../assets/vene.png";
 import Conmebol from "../assets/conmebol.png";
+import Bolivia from "../assets/bolivia.png";
 
 const fixtures = [
   {
@@ -13,7 +14,7 @@ const fixtures = [
     awayTeam: {
       name: "Venezuela",
       logo: Vene,
-      country: "Brazil",
+      country: "Venezuela",
     },
     date: "2024-10-11",
     time: "04:45",
@@ -21,6 +22,25 @@ const fixtures = [
     comp: "WC - Qualifiers Conmebol",
     venue: "Estadio Monumental de Maturín",
     stadium: "Venezuela",
+    competetion: Conmebol,
+  },
+  {
+    homeTeam: {
+      name: "Argentina",
+      logo: Argentina,
+      country: "Argentina",
+    },
+    awayTeam: {
+      name: "Bolivia",
+      logo: Bolivia,
+      country: "Bolivia",
+    },
+    date: "2024-10-16",
+    time: "05:45",
+    format: "AM",
+    comp: "WC - Qualifiers Conmebol",
+    venue: "Estadio Mas Monumental",
+    stadium: "Buenos Aires, Argentina",
     competetion: Conmebol,
   },
 
